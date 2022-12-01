02d71d0024e541817ce3cfc79affcf87aeb7c2c3
damienn7 <118727505+damienn7@users.noreply.github.com>
2022-12-01 10:01:20 +0100

    push de l'exercice 1

---
+++ ex_01.php
@@ -0,0 +1,30 @@
+<?php
+
+function which_is_longer($str1, $str2)
+{
+    if((!isset($str1) || !isset($str2)) || (!is_string($str1)||!is_string($str2)))
+    {
+        return -1;
+    }
+
+    if(strlen($str1)==0||strlen($str2)==0)
+    {
+        return -1;
+    }
+
+    if(strlen($str1)==strlen($str2))
+    {
+        return strlen($str1);
+    }
+
+    if(strlen($str1)>strlen($str2))
+    {
+        return strlen($str1);
+    }
+    elseif(strlen($str1)<strlen($str2))
+    {
+        return strlen($str2);
+    }
+}
+
+echo which_is_longer("fejfzlzhef");
